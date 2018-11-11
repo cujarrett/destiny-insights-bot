1c2dd381ffa18ca5749ff665cf578e91449bfa1b
Matt Jarrett <cujarrett@gmail.com>
2018-11-11 05:15:29 -0600

    Updates port to 3000

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const twitterBotConfig = {
 /* eslint-disable camelcase*/
 
 const main = async () => {
-  process.env.PORT || 5000
+  process.env.PORT || 3000
   momentTimezone.tz.setDefault("America/Chicago")
 
   setInterval(async () => {
