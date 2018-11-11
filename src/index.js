f6186c8acfb2cb3c4de3b4e63ed0d0e32af116bb
Matt Jarrett <cujarrett@gmail.com>
2018-11-11 03:29:01 -0600

    Adds Twitter integration

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
+const moment = require("moment")
+const momentTimezone = require("moment-timezone")
+const twit = require("twit")
 const braytech = require("./integrations/braytech.js")
+const { twitterConsumerApiKey, twitterConsumerSecret, twitterAccessToken, twitterAccessTokenSecret } = require("./settings.js")
+
+const twitterBotConfig = {
+  consumer_key: twitterConsumerApiKey,
+  consumer_secret: twitterConsumerSecret,
+  access_token: twitterAccessToken,
+  access_token_secret: twitterAccessTokenSecret,
+  timeout_ms: 60*1000,
+  strictSSL: true
+}
 
 const main = async () => {
-  const response = await braytech.getModsForSale()
-  // Allow app to log mods
-  // eslint-disable-next-line no-console
-  console.log(response)
+  momentTimezone.tz.setDefault("America/Chicago")
+
+  setInterval(async () => {
+    const timestamp = moment().format("hh:mm a")
+
+    if (timestamp === "11:01 am") {
+      const { mods } = await braytech.getModsForSale()
+      const firstMod = mods[0]
+      const secondMod = mods[1]
+
+      const twitter = new twit(twitterBotConfig)
+      const tweet = `Banshee-44 is selling ${firstMod} and ${secondMod} today. #Destiny2 #TwitterBot`
+
+      twitter.post("statuses/update", { status: tweet })
+      console.log(`${timestamp} Tweeted: ${tweet}`)
+    }
+  }, 60 * 1000)
 }
 
 try {
