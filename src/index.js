519bda506bc2e160042c8ad183613bf43aaefa49
Matt Jarrett <cujarrett@gmail.com>
2018-11-13 21:48:47 -0600

    Continues server side testing

--- src/index.js
+++ src/index.js
@@ -16,13 +16,20 @@ const tweetBot = async () => {
   momentTimezone.tz.setDefault("UTC")
 
   try {
-    const { mods } = await braytech.getModsForSale()
-    const [firstMod, secondMod] = mods
-    const timestamp = moment().format("YYYY-DD-MM, hh:mm:ss a")
-    // Allow tweet to be longer than 100 characters
-    // eslint-disable-next-line max-len
-    const message = `${timestamp} Banshee-44 is selling ${firstMod} and ${secondMod} today. #Destiny2 #TwitterBot`
-    twitter.post(message)
+    const currentTime = moment().format("hh:mm a")
+
+    console.log({currentTime})
+
+    // if (currentTime === "11:02 am") {
+      const { mods } = await braytech.getModsForSale()
+      const [firstMod, secondMod] = mods
+      // TODO remove timestamp for server side testing
+      const timestamp = moment().format("YYYY-DD-MM, hh:mm:ss a")
+      // Allow tweet to be longer than 100 characters
+      // eslint-disable-next-line max-len
+      const message = `${timestamp} Banshee-44 is selling ${firstMod} and ${secondMod} today. #Destiny2 #TwitterBot`
+      twitter.post(message)
+    // }
   } catch (error) {
     const timestamp = moment().format("YYYY-DD-MM, hh:mm:ss a")
     console.log(`${timestamp} - ${error}`)
