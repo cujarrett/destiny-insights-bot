ae12a8a2880c28547033ebeb479d885a485e4c87
Matt Jarrett <cujarrett@gmail.com>
2018-11-13 22:08:14 -0600

    Adds temp server side test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-// const express = require("express")
-// const fetch = require("node-fetch")
 const moment = require("moment")
 const momentTimezone = require("moment-timezone")
 const braytech = require("./integrations/braytech.js")
 const twitter = require("./integrations/twitter.js")
-
-// const port = process.env.PORT || 3000
-// const app = express()
-
-// app.listen(port, () => {
-//   console.log(`Server started on port ${port}`)
-// })
 
 const tweetBot = async () => {
   momentTimezone.tz.setDefault("UTC")
@@ -18,18 +9,20 @@ const tweetBot = async () => {
   try {
     const currentTime = moment().format("hh:mm a")
 
-    console.log({currentTime})
+    console.log({ currentTime })
 
-    // if (currentTime === "11:02 am") {
+    if (currentTime === "11:02 am" || currentTime === "04:31 am") {
       const { mods } = await braytech.getModsForSale()
       const [firstMod, secondMod] = mods
+
       // TODO remove timestamp for server side testing
       const timestamp = moment().format("YYYY-DD-MM, hh:mm:ss a")
+
       // Allow tweet to be longer than 100 characters
       // eslint-disable-next-line max-len
       const message = `${timestamp} Banshee-44 is selling ${firstMod} and ${secondMod} today. #Destiny2 #TwitterBot`
       twitter.post(message)
-    // }
+    }
   } catch (error) {
     const timestamp = moment().format("YYYY-DD-MM, hh:mm:ss a")
     console.log(`${timestamp} - ${error}`)
