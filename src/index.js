404900dcb434e847e1948decb558df92a8b3ea4d
Matt Jarrett <cujarrett@gmail.com>
2018-11-14 18:36:05 -0600

    Removes time gating from tweet function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 const moment = require("moment")
-const momentTimezone = require("moment-timezone")
 const braytech = require("./integrations/braytech.js")
 const twitter = require("./integrations/twitter.js")
 
 const tweetBot = async () => {
-  momentTimezone.tz.setDefault("UTC")
-
   try {
-    const currentTime = moment().format("hh:mm a")
+    const { mods } = await braytech.getModsForSale()
+    const [firstMod, secondMod] = mods
 
-    if (currentTime === "05:00 pm") {
-      const { mods } = await braytech.getModsForSale()
-      const [firstMod, secondMod] = mods
-
-      // Allow tweet to be longer than 100 characters
-      // eslint-disable-next-line max-len
-      const message = `Banshee-44 is selling ${firstMod} and ${secondMod} today. #Destiny2 #TwitterBot`
-      twitter.post(message)
-    }
+    // Allow tweet to be longer than 100 characters
+    // eslint-disable-next-line max-len
+    const message = `Banshee-44 is selling ${firstMod} and ${secondMod} today. #Destiny2 #TwitterBot`
+    twitter.post(message)
   } catch (error) {
     const timestamp = moment().format("YYYY-DD-MM, hh:mm:ss a")
     console.log(`${timestamp} - ${error}`)
