f803635c216edc5e5cd1ee195412edf354834fd3
Matt Jarrett <cujarrett@gmail.com>
2019-12-30 13:47:56 -0600

    feat: migrating to aws and circle ci

---
+++ index.js
@@ -0,0 +1,24 @@
+const braytech = require("./src/integrations/braytech.js")
+const twitter = require("./src/integrations/twitter.js")
+
+exports.handler = async (event, context, callback) => {
+  try {
+    const mods = await braytech.getModsForSale()
+    const [firstMod, secondMod] = mods
+    // Allow tweet to be longer than 100 characters
+    // eslint-disable-next-line max-len
+    const message = `Banshee-44 is selling ${firstMod} and ${secondMod} today. #Destiny2 #TwitterBot`
+    await twitter.post(message)
+
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(`${message} posted.`)
+    }
+    context.callbackWaitsForEmptyEventLoop = false
+    callback(null)
+    return response
+  } catch (error) {
+    context.callbackWaitsForEmptyEventLoop = false
+    callback(new Error(error), "banshee-44-mods-bot error thrown")
+  }
+}
--- src/integrations/braytech.js
+++ src/integrations/braytech.js
@@ -1,30 +1,23 @@
-const prettyMs = require("pretty-ms")
 const fetch = require("node-fetch")
-const StopWatch = require("statman-stopwatch")
 
 module.exports.getModsForSale = async () => {
   const endpoint = "https://voluspa.braytech.org/vendor/?hash=672118013&defined=true"
-
   const options = {
     method: "GET",
     json: true
   }
 
-  const stopwatch = new StopWatch(true)
-  const rawResponse = await fetch(endpoint, options)
-  const response = await rawResponse.json()
-  const vendorData = response.Response.sales.data
+  try {
+    const rawResponse = await fetch(endpoint, options)
+    const response = await rawResponse.json()
+    const vendorData = response.Response.sales.data
 
-  const modsData = Object.values(vendorData).filter((vendorItem) => {
-    return vendorItem.itemDefinition.itemType === 19
-  })
-
-  stopwatch.stop()
-  const braytechProcessTime = prettyMs(stopwatch.time())
-  const mods = modsData.map((modData) => modData.itemDefinition.displayProperties.name)
-
-  return {
-    mods,
-    braytechProcessTime
+    const modsData = Object.values(vendorData).filter((vendorItem) => {
+      return vendorItem.itemDefinition.itemType === 19
+    })
+    const mods = modsData.map((modData) => modData.itemDefinition.displayProperties.name)
+    return mods
+  } catch (error) {
+    throw new Error("Braytech is not available or not working.")
   }
 }
