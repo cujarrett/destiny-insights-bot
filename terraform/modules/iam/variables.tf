91e86f5506059538c5509947c52575e6de6ea0fe
Matt Jarrett <cujarrett@gmail.com>
2020-12-11 20:26:41 -0600

    :wrench: Migrates to AWS Param Store from AWS Secrets Manager

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -7,3 +7,8 @@ variable "parameter-store-twitter-auth-arn" {
   description = "Parameter Store twitter auth config path ARN"
   type = string
 }
+
+variable "aws-dynamodb-table-banshee-44-mods-bot-arn" {
+  description = "DynamoDB table ARN"
+  type = string
+}
