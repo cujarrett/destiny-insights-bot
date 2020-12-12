92f7e5cf54ff6264c3e6cb565b5b96dbfdbc538f
Matt Jarrett <cujarrett@gmail.com>
2020-12-11 20:25:07 -0600

    :wrench Migrates to AWS Param Store from AWS Secrets Manager

--- terraform/modules/iam/variables.tf
+++ terraform/modules/iam/variables.tf
@@ -3,11 +3,6 @@ variable "error-sns-topic" {
   type = string
 }
 
-variable "parameter-store-twitter-access-token-arn" {
-  description = "Parameter Store twitter-access-token ARN"
-  type = string
-}
-
 variable "parameter-store-twitter-auth-arn" {
   description = "Parameter Store twitter auth config path ARN"
   type = string
