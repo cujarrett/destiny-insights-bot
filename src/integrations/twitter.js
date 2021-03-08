a9fdbe51b0915f366acb00e5e525e2128c5e1f6b
Matt Jarrett <cujarrett@gmail.com>
2021-03-07 19:32:48 -0600

    feat: updates tweet trigger logic

--- src/integrations/twitter.js
+++ src/integrations/twitter.js
@@ -1,24 +1,7 @@
 const Twit = require("twit")
 const { getParameters } = require("./aws-parameter-store.js")
 
-module.exports.test = async (message) => {
-  // eslint-disable-next-line max-len
-  const { TWITTER_TEST_CONSUMER_API_KEY, TWITTER_TEST_CONSUMER_SECRET, TWITTER_TEST_ACCESS_TOKEN, TWITTER_TEST_ACCESS_TOKEN_SECRET } = require("../settings.js")
-  // Allow Twit mandated use of _ in object keys
-  /* eslint-disable camelcase*/
-  const twitterBotConfig = {
-    consumer_key: TWITTER_TEST_CONSUMER_API_KEY,
-    consumer_secret: TWITTER_TEST_CONSUMER_SECRET,
-    access_token: TWITTER_TEST_ACCESS_TOKEN,
-    access_token_secret: TWITTER_TEST_ACCESS_TOKEN_SECRET,
-    timeout_ms: 60 * 1000,
-    strictSSL: true
-  }
-
-  return await post(message, twitterBotConfig)
-}
-
-module.exports.tweet = async (message) => {
+const getTwitterBotConfig = async () => {
   // eslint-disable-next-line max-len
   const { TWITTER_CONSUMER_API_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET } = await getParameters()
   // Allow Twit mandated use of _ in object keys
@@ -31,7 +14,32 @@ module.exports.tweet = async (message) => {
     timeout_ms: 60 * 1000,
     strictSSL: true
   }
+  return twitterBotConfig
+}
 
+const getTestTwitterBotConfig = () => {
+  // eslint-disable-next-line max-len
+  const { TWITTER_TEST_CONSUMER_API_KEY, TWITTER_TEST_CONSUMER_SECRET, TWITTER_TEST_ACCESS_TOKEN, TWITTER_TEST_ACCESS_TOKEN_SECRET } = require("../settings.js")
+  // Allow Twit mandated use of _ in object keys
+  /* eslint-disable camelcase*/
+  const twitterBotConfig = {
+    consumer_key: TWITTER_TEST_CONSUMER_API_KEY,
+    consumer_secret: TWITTER_TEST_CONSUMER_SECRET,
+    access_token: TWITTER_TEST_ACCESS_TOKEN,
+    access_token_secret: TWITTER_TEST_ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000,
+    strictSSL: true
+  }
+  return twitterBotConfig
+}
+
+module.exports.test = async (message) => {
+  const twitterBotConfig = getTestTwitterBotConfig()
+  return await post(message, twitterBotConfig)
+}
+
+module.exports.tweet = async (message) => {
+  const twitterBotConfig = await getTwitterBotConfig()
   await post(message, twitterBotConfig)
 }
 
@@ -43,4 +51,34 @@ const post = async (message, twitterBotConfig) => {
   } catch (error) {
     throw new Error(`Twitter Error from trying to tweet ${message} - ${error}`)
   }
+}
+
+module.exports.getLastModTweetDate = async () => {
+  const twitterBotConfig = await getTwitterBotConfig()
+  const twitter = new Twit(twitterBotConfig)
+  const queryOptions = { screen_name: "banshee44mods", count: 10 }
+  const response = await twitter.get("statuses/user_timeline", queryOptions)
+  const tweets = response.data
+
+  const timeline = []
+  for (const tweet of tweets) {
+    timeline.push({ timestamp: tweet.created_at, message: tweet.text })
+  }
+
+  const sortedTweets = timeline.sort((first, second) => {
+    return new Date(second.timestamp) - new Date(first.timestamp)
+  })
+
+  const pastModTweets = []
+  for (const tweet of sortedTweets) {
+    if (tweet.message.startsWith("Banshee-44 is selling:")) {
+      pastModTweets.push(new Date(tweet.timestamp))
+    }
+  }
+
+  const sortedModTweets = pastModTweets.sort((first, second) => {
+    return new Date(second.timestamp) - new Date(first.timestamp)
+  })
+
+  return sortedModTweets[0]
 }
